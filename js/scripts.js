07e73f86ea7d29ec29dee72a2b56c8936eaa161a
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2020-09-27 17:41:39 -0500

    Updated scripts.js
    Application set-up; setting up Pokémon data to display.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert("Hello World");
-
-let favoriteFood = "steak";
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Squirtle', height: 0.5, types: ['water', 'monster']}
+  {name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']}
+  {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']}
+  {name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']}
+  {name: 'Onix', height: 8.8, types: ['rock', 'ground', 'mineral']}
+];
