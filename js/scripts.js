d8200f2fdf58b8449195efa697dc02ab2ff84e5c
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2020-09-29 23:35:06 -0500

    Updated scripts.js
    reverted changes back to 1.1 exercise for approval

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-let pokemonList = [
-  {name: 'Squirtle', height: 0.5, types: ['water', 'monster']}
-  {name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']}
-  {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']}
-  {name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']}
-  {name: 'Onix', height: 8.8, types: ['rock', 'ground', 'mineral']}
-];
+alert('Hello World');
+
+let favoriteFood = 'steak';
+document.write(favoriteFood);
