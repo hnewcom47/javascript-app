82b9220bf4f1bde827851f7044282f38c1524cda
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2020-10-05 22:14:24 -0500

    Updated scripts.js
    Created a for loop that iterates over pokemon's name and height, as well as a conditional that checks pokemon's height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-alert('Hello World');
+let pokemonList = [
+  {name: 'Squirtle', height: 0.5, types: ['water', 'monster']},
+  {name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']},
+  {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']},
+  {name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
+  {name: 'Onix', height: 8.8, types: ['rock', 'ground', 'mineral']}
+];
 
-let favoriteFood = 'steak';
-document.write(favoriteFood);
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 4) {
+    document.write ('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow! That\'s big!' + '</p>');
+  } else {
+      document.write ('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+  }
+}
