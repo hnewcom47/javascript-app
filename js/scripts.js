867f9e1d7f4e67ae01996f8a8be8912efdf095cf
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2020-10-10 16:16:51 -0500

    Updated scripts.js
    Wrapped pokemonList array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-let pokemonList = [
-  {name: 'Squirtle', height: 0.5, types: ['water', 'monster']},
-  {name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']},
-  {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']},
-  {name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
-  {name: 'Onix', height: 8.8, types: ['rock', 'ground', 'mineral']}
-];
+(function () {
+  let pokemonList = [
+    {name: 'Squirtle', height: 0.5, types: ['water', 'monster']},
+    {name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']},
+    {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']},
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
+    {name: 'Onix', height: 8.8, types: ['rock', 'ground', 'mineral']}
+  ];
+})();
 
 pokemonList.forEach (function (pokemon) {
   document.write (pokemon.name + ': ' + pokemon.height + ' meters, ' + 'type: ' + pokemon.types + '<br>');
